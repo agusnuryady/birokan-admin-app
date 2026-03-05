df97634302af0f93cd03e0b4e18286ab58bf9a44
Rahmat Agus Nuryady <agusxlciti@gmail.com>
2026-03-05 19:40:30 +0700

    fix: build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,20 @@
 import bundleAnalyzer from '@next/bundle-analyzer';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
 
 // Replicate __dirname for ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
 
 export default withBundleAnalyzer({
-  turbopack: {
-    // Now __dirname is defined and safe to use
-    root: __dirname,
-  },
+  // turbopack: {
+  //   // Now __dirname is defined and safe to use
+  //   root: __dirname,
+  // },
   reactStrictMode: false,
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
